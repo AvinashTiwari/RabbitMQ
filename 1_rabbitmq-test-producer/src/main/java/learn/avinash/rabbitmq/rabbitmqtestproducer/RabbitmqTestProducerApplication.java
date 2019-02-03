4cbe16322819d6efe27385009a44c66e0604f996
avinash\avinash.t <qwe123kids@gmail.om>
2019-02-03 15:42:20 -0700

    Rabbit sample

--- 1_rabbitmq-test-producer/src/main/java/learn/avinash/rabbitmq/rabbitmqtestproducer/RabbitmqTestProducerApplication.java
+++ 1_rabbitmq-test-producer/src/main/java/learn/avinash/rabbitmq/rabbitmqtestproducer/RabbitmqTestProducerApplication.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RabbitmqTestProducerApplication implements CommandLineRunner {
 
 	@Autowired
 	private HelloRabbitProducer helloRabbitProducer;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqTestProducerApplication.class, args);
